0b814f2a92416636e6e483c64a76571140fa6f9b
Somesh Acharya <somesh.acharya@hmcts.net>
2020-10-01 17:31:55 +0100

    DTSBP-109: Unique key to rejecting duplicate print request

---
+++ src/main/resources/db/migration/V018__Add_unique_key_checksum_status.sql
@@ -0,0 +1,9 @@
+update letters let set checksum = updateQuery.checksum from
+(select counter, checksum || counter checksum, status, id from (
+select row_number() over (partition by checksum, status) counter, checksum, status, id
+from letters) duplicate
+where duplicate.counter > 1) updateQuery
+where let.id = updateQuery.id;
+
+alter table letters
+add constraint checksum_status unique (checksum, status);
