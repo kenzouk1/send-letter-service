af778300cbd9f9c27150c81aab642ce1335b0f3e
Somesh Acharya <somesh.acharya@hmcts.net>
2020-10-06 14:26:55 +0100

    Revert "DTSBP-109: Added confict integration for hmtl template

--- src/functionalTest/java/uk/gov/hmcts/reform/sendletter/ProcessMessageTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sendletter/ProcessMessageTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.sendletter.controllers.MediaTypes;
 
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -61,14 +63,36 @@ class ProcessMessageTest extends FunctionalTestSuite {
         });
     }
 
+    @Test
+    void should_send_letter_and_upload_file_on_sftp_server_when_letter_contains_two_documents_with_odd_pages()
+        throws Exception {
+        String letterId = sendPrintLetterRequest(
+            signIn(),
+            sampleLetterRequestJson("letter_two_documents.json", "one-page-template.html")
+        );
+
+        awaitAndVerifyFileOnSftp(letterId, (sftpFile, sftp) -> {
+            assertThat(sftpFile.getName()).matches(getFileNamePattern(letterId));
+
+            if (!isEncryptionEnabled) {
+                validatePdfFile(letterId, sftp, sftpFile, 4);
+            }
+        });
+    }
+
+    @Test
+    void should_throw_ConflictException()  {
+        executeMutiReques(this::getLetterRequest);
+    }
+
     private String getLetterRequest() {
         String letterId = "none";
         try {
-            letterId = sendPrintLetterRequest(
+            letterId =  sendPrintLetterRequest(
                     signIn(),
-                    sampleLetterRequestJson("letter_single_document.json", "two-page-template.html")
+                    samplePdfLetterRequestJson("letter_single_document.json", "one-page-template_duplicate")
             );
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return letterId;
